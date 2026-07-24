4267561f610f0e71f1829e9eb5e0fcd069d3a572
Võ Văn Hùng <vohung.it2@gmail.com>
2026-07-24 22:07:44 +0700

    add

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isGithubActions ? "/santoni" : "",
-  assetPrefix: isGithubActions ? "/santoni/" : "",
+  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
